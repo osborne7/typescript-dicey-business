b27333d329ace3c1ba687ac5c908a9bdf07bc72c
osborne7 <eleanor.c.osborne@gmail.com>
2020-06-25 18:20:18 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+let button = document.getElementById('generate-die');
+let container = document.getElementById('container');
+let newVal;
+let rollButton = document.getElementById('roll-die');
+
+class Die {
+    constructor() {
+        this.div = document.createElement('div');
+        this.div.className = 'die';
+        this.roll();
+    }
+    roll = () => {
+        let newVal = this.randomNumber();
+        console.log(newVal);
+        this.div.innerHTML = newVal;
+        container.appendChild(this.div);
+        // return newVal;
+    }
+    randomNumber = () => Math.floor(Math.random() * Math.floor((6-1) + 1));
+}
+
+class allDie extends Die {
+    constructor() {
+        super();
+        this.roll();
+    }
+    roll = () => {
+        this.div = document.getElementsByClassName('die');
+        for (let i = 0; i<this.div.length; i++) {
+            this.div[i].innerHTML = this.randomNumber();
+            }
+    }
+}
+
+button.addEventListener('click', () => {
+    new Die();
+})
+
+rollButton.addEventListener('click', () => {
+    new allDie();
+})
